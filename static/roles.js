26258c70d6e91b1df296352ebd74b048b45f899e
Scovia Achan <achanscovia@gmail.com>
2021-06-19 17:00:26 +0300

    create middleware to check user role organise folder structure

---
+++ static/roles.js
@@ -0,0 +1,12 @@
+module.exports = {
+    admin : {
+        update_parcel_status: true,
+        get_all_parcels: true,
+    },
+    buyer : {
+        get_parcels: true,
+        update_destination: true,
+        get_single_parcel: true,
+        create_delivery_order: true
+    }
+}
