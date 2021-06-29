b15340bc2a837758582ce22df362ecd7440b1daf
Scovia Achan <achanscovia@gmail.com>
2021-06-29 09:35:43 +0300

    clean up backend routes

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const {createUsers, loginUser, logout} = require("../Controllers/users")
-const {isAuth} = require("../middlewares")
+const {getAllUsers,createUsers, loginUser, logout} = require("../Controllers/users")
+const {isAuth, checkPermissions} = require("../middlewares")
 
 
-
+router.get("/", [checkPermissions.can("get_all_users"),getAllUsers])
 router.post("/signup", createUsers)
 router.post("/login",loginUser)
 router.get("/logout", logout)
-
 
 
 module.exports = router;
--- static/roles.js
+++ static/roles.js
@@ -1,12 +1,18 @@
 module.exports = {
     admin : {
-        update_parcel_status: true,
+        update_status: true,
         get_all_parcels: true,
+        update_currentLocation: true,
+        get_all_users: true,
+        get_single_parcel: true
+        
     },
     user : {
-        get_parcels: true,
+        get_parcels: false,
         update_destination: true,
         get_single_parcel: true,
-        create_delivery_order: true
+        create_delivery_order: true,
+        update_currentLocation: false,
+        get_all_users: false
     }
 }
